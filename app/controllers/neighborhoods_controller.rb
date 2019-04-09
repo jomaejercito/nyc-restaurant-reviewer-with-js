ee794a319626370dc9bc79f018db84d9f080d48c
Joma Ejercito <jomaejercito@gmail.com>
2019-04-09 18:50:52 -0400

    Add respond_to to neighborhoods controller to respond to json

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -3,10 +3,18 @@ class NeighborhoodsController < ApplicationController
 
   def index
     @neighborhoods = Neighborhood.all
+    respond_to do |f|
+      f.html {render :index}
+      f.json {render json: @neighborhoods}
+    end
   end
 
   def show
     @neighborhood = Neighborhood.find(params[:id])
+    respond_to do |f|
+      f.html {render :show}
+      f.json {render json: @neighborhood}
+    end
   end
 
 end
