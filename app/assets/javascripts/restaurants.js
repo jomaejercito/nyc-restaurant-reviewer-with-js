8c4eb7646e19ceb513e803a7274094b12397d8af
Joma Ejercito <jomaejercito@gmail.com>
2019-06-16 04:42:33 -0400

    Remove highest_rated.js and place all code into restaurants.js

---
+++ app/assets/javascripts/restaurants.js
@@ -0,0 +1,53 @@
+$(() => {
+  bindClickHandlers()
+})
+
+const bindClickHandlers = () => {
+  $('.js-high').on('click', (e) => {
+     e.preventDefault()
+     // history.pushState(null, null, "restaurants/highest_rated")
+     fetch(`/restaurants/highest_rated.json`)
+      .then(response => response.json())
+      .then(restaurants => {
+        $('.js-high').html('')
+        restaurants.forEach(restaurant => {
+          let newRestaurant = new Restaurant(restaurant)
+          let restaurantHtml = newRestaurant.formatIndex()
+          $('.js-high').append(restaurantHtml)
+          console.log(newRestaurant)
+        })
+      })
+  })
+  $('.js-pop').on('click', (e) => {
+     e.preventDefault()
+     // history.pushState(null, null, "restaurants/highest_rated")
+     fetch(`/restaurants/popular.json`)
+      .then(response => response.json())
+      .then(restaurants => {
+        $('.js-pop').html('')
+        restaurants.forEach(restaurant => {
+          let newRestaurant = new Restaurant(restaurant)
+          let restaurantHtml = newRestaurant.formatIndex()
+          $('.js-pop').append(restaurantHtml)
+          console.log(newRestaurant)
+        })
+      })
+  })
+}
+
+function Restaurant(restaurant) {
+  this.id = restaurant.id
+  this.name = restaurant.name
+  this.reviews = restaurant.reviews
+  this.users = restaurant.users
+  this.cuisine = restaurant.cuisine
+  this.neighborhood = restaurant.neighborhood
+  // this.average_rating = restaurant.average_rating
+}
+
+Restaurant.prototype.formatIndex = function() {
+  let restaurantHtml = `
+    <h4>${this.name}</h4>
+  `
+  return restaurantHtml
+}
