21c89dd4b12afe52d3e368a9a2520f4ba83fe856
Joma Ejercito <jomaejercito@gmail.com>
2019-07-20 23:16:14 +0800

    Add class and id on review form

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,31 +1,31 @@
-// $(() => {
-//   $(document).on("submit", '.form-group', function(e){
-//     e.preventDefault()
-//
-//     const values = ($(this).serialize())
-//
-//     $.review(`/users/${this.userID}/reviews/`, values).done(function(data){
-//       $('#main').html('')
-//       const newReview = new Review(data)
-//       const htmlToAdd = newReview.formatShow()
-//       $('#main').html(htmlToAdd)
-//     })
-//   })
-// })
-//
-// function Review(review) {
-//   this.id = review.id
-//   this.name = review.restaurant.name
-//   this.comment = review.comment
-//   this.rating = review.rating
-//   this.userID = review.user.id
-// }
-//
-// Review.prototype.formatShow = function(){
-//   let htmlToAdd = `
-//     <h1>My Review of ${this.restaurant.name}</h1><br>
-//     <p>${this.comment}</p>
-//     <p>${this.rating}</p>
-//   `
-//   return htmlToAdd
-// }
+$(() => {
+  $("#new-form").on("submit", function(e){
+    e.preventDefault()
+
+    console.log($(this).serialize())
+
+  //   $.post(`/users/${this.user}/reviews/`, values).done(function(data){
+  //     $('#main').html('')
+  //     const newReview = new Review(data)
+  //     const htmlToAdd = newReview.formatShow()
+  //     $('#main').html(htmlToAdd)
+  //   })
+  })
+})
+
+function Review(review) {
+  this.id = review.id
+  this.name = review.restaurant.name
+  this.comment = review.comment
+  this.rating = review.rating
+  this.user = review.user
+}
+
+Review.prototype.formatShow = function(){
+  let htmlToAdd = `
+    <h1>My Review of ${this.restaurant.name}</h1><br>
+    <p>${this.comment}</p>
+    <p>${this.rating}</p>
+  `
+  return htmlToAdd
+}
